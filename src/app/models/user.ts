38032acbdbf9495960cd941ac7c3d96d1ca033a3
belkoweb <belkoweb9718@gmail.com>
2021-07-26 15:11:16 +0100

    first commit

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id:number;
+  firstName:string;
+  lastName:string;
+  country:string;
+  phoneNumber:string;
+  postalCode:string;
+  birth:string;
+  email:string;
+  password:string;
+}
